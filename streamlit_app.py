82664b5a50c7a8c6a0b9b176b21480381395400b
devauxje314 <147081296+devauxje314@users.noreply.github.com>
2023-10-10 16:39:09 +0200

    Update streamlit_app.py
    remove json text and changed url to display kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 
 streamlit.title('My Parents New Healthy Dinner')
@@ -25,8 +25,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
-
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
